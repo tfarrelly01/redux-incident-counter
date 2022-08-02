1cdf11517f44e5ff093552b8b53b36c90fd6bff4
Tony Farrelly <tfarrelly01@gmail.com>
2022-08-02 13:04:03 +0100

    Add enhancer to store for Redux dev tools

--- src/store.js
+++ src/store.js
@@ -3,4 +3,4 @@ import { reducer } from './reducer';
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, enhancer);
